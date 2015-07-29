bfafe12407b864ea2be6692a93d00516d7d1b667
Brian M Williams <bmwilli1@comcast.net>
2015-07-29 08:05:01 -0400

    Begin to integrate jcommander for command line args parsing

--- src/main/java/bmwilli/streamsjmx/Client.java
+++ src/main/java/bmwilli/streamsjmx/Client.java
@@ -9,21 +9,47 @@ import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
 import com.ibm.streams.management.domain.DomainMXBean;
 import com.ibm.streams.management.ObjectNameBuilder;
 
-// javac -cp com.ibm.streams.management.jmxmp.jar:com.ibm.streams.management.mx.jar Client.java
-// java -cp .;com.ibm.streams.management.jmxmp.jar:com.ibm.streams.management.mx.jar:jmxremote_optional.jar 
-//   Client service:jmx:jmxmp://server:9975 domainA user password
-// Note: It is important to include com.ibm.streams.management.jmxmp.jar in the class path before jmxremote_optional.jar
+// java -jar xxx service:jmx:jmxmp://server:9975 domainA user password
+
+
+
 public class Client {
   public static void main(String [] args) {
     try { 
-      String jmxUrl = args[0];  // use streamtool getjmxconnect to find
-      String domainName = args[1];
-      String user = args[2];    // use streamtool setacl or streamtool setdomainacl to assign required permissions
-      String password = args[3];
-      //System.out.println("jmxURL: " + jmxUrl + ", domainName: " + domainName + ", user: " + user + ", password: " + password);
+
+      //String jmxUrl = args[0];  // use streamtool getjmxconnect to find
+      //String domainName = args[1];
+      //String user = args[2];    // use streamtool setacl or streamtool setdomainacl to assign required permissions
+      //String password = args[3];
+
+      // Parse command line arguments
+      CommandLineArgs cla = new CommandLineArgs();
+      //String[] argv = {};
+      JCommander cmd = new JCommander(cla);
+
+      try {
+        cmd.parse(args);
+        System.out.println("jmxURL: " + cla.jmxUrl + ", domainName: " + cla.domainName + ", user: " + cla.user + ", password: " + cla.password);
+      } catch (ParameterException ex) {
+        System.out.println(ex.getMessage());
+        cmd.usage();
+        System.exit(1);
+      }
+
+
+
+      String jmxUrl = cla.jmxUrl;
+      String domainName = cla.domainName;
+      String user = cla.user;
+      String password = cla.password;
+
       HashMap<String,Object> env = new HashMap<String,Object>();
       String [] creds = {user, password};
       env.put("jmx.remote.credentials", creds);
